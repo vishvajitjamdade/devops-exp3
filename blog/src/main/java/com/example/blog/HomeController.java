e6f597bebac4036fc180f92217ece48b06c5fb6c
VishvajitJamdade01 <vishvajitjamdade25@gmail.com>
2025-11-18 09:08:23 +0530

    Initial blog app

---
+++ blog/src/main/java/com/example/blog/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/post")
+    public String post() {
+        return "post";
+    }
+}
